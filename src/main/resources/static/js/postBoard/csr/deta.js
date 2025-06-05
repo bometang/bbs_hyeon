cdc6a623e755f8e3ed0fd2b38ed34bbd2a091a3c
hyeonjun lee <ihyeonjun117@gmail.com>
2025-06-05 16:58:00 +0900

    new comment

--- src/main/resources/static/js/postBoard/csr/deta.js
+++ src/main/resources/static/js/postBoard/csr/deta.js
@@ -268,175 +268,84 @@ const getPostComment = async (reqPage, reqRec) => {
   }
 };
 
-function displayReadForm() {
+//댓글목록 화면
+function displayPostCommentList(postComment) {
+
   //상태 : 조회 mode-read, 편집 mode-edit
-  const changeEditModeComment = (cid,frm) => {
-    frm.classList.toggle('mode-edit-comment', true);
-    frm.classList.toggle('mode-read-comment', false);
+  const changeEditModeComment = frm => {
+    frm.classList.toggle('mode-edit-Comment', true);
+    frm.classList.toggle('mode-read-Comment', false);
+    [...frm.querySelectorAll('input,textarea')]
+      .filter(input => !['postId', 'nickname', 'cdate', 'udate'].includes(input.name))
+      .forEach(input => input.removeAttribute('readonly'));
 
-      // 기존 다른 댓글의 버튼 비우기
-      frm.querySelectorAll('.btns').forEach($td => {
-        if ($td.dataset.cid !== cid) {
-          $td.innerHTML = '';
-        }
-      });
-
-    const $label = frm.querySelector(`label.comment-content[data-cid="${cid}"]`);
-    const text = $label.textContent;
-
-      // input 요소 생성
-    const $input = document.createElement('input');
-    $input.type = 'text';
-    $input.name = 'content';
-    $input.value = text;
-    $input.classList.add('input-comment');
-
-    $label.replaceWith($input);
-
-    const $btnsCell = frm.querySelector(`.btns[data-cid="${cid}"]`);
-    $btnsCell.innerHTML = `
-      <button id="btnCommentSave" type="button">저장</button>
-      <button id="btnCommentCancel" type="button">취소</button>
+    const $btns = frm.querySelector('.btns');
+    $btns.innerHTML = `
+      <button id="btnSave" type="button">저장</button>
+      <button id="btnCancel" type="button">취소</button>
     `;
 
-    const $btnSave = $btns.querySelector('#btnCommentSave');
-    const $btnCancel = $btns.querySelector('#btnCommentCancel');
+    const $btnSave = $btns.querySelector('#btnSave');
+    const $btnCancel = $btns.querySelector('#btnCancel');
 
     //저장
-    $btnSave.addEventListener('click', e => {
-      const content = frm.querySelector('input[name="content"]').value;
-      const commentId = frm.querySelector('input[name="commentId"]').value;
+    $btnSave.onclick = async e => {
+      frm.querySelector('#errTitle').textContent = '';
+      frm.querySelector('#errContent').textContent = '';
 
-      modifyPostComment(pid, commentId, postComment); //수정
-      changeReadModeComment(frm); //읽기모드
-    });
+      const formData = new FormData(frm); //폼데이터가져오기
+      const postBoard = {};
+
+      [...formData.keys()].forEach(
+        ele => (postBoard[ele] = formData.get(ele))
+      );
+
+      const result = await modifyPostBoard(pid, postBoard);
+
+      if (result.header.rtcd.startsWith('E')) {
+        const details = result.header.details;
+        if (details.title)  frm.querySelector('#errTitle').textContent   = details.title;
+        if (details.content) frm.querySelector('#errContent').textContent = details.content;
+        return;
+      }
+      const udate = result.body.udate;
+      frm.querySelector('#udate"]').value = udate; //수정
+      changeReadMode(frm); //읽기모드
+    };
 
     //취소
     $btnCancel.addEventListener('click', e => {
       frm.reset(); //초기화
-      changeReadModeComment(frm);
+      changeReadMode(frm);
     });
   };
 
-  const changeReadMode = frm => {
-    frm.classList.toggle('mode-read-comment', true);
-    frm.classList.toggle('mode-edit-comment', false);
-
-  // 기존 다른 댓글의 버튼 비우기
-    frm.querySelectorAll('.btns').forEach($td => {
-      $td.innerHTML = `
-         <button id="btnCommentSave" type="button">수정</button>
-         <button id="btnCommentCancel" type="button">삭제</button>
-      `;
-    });
-
-    const $btns = frm.querySelector('.btns');
-    $btns.innerHTML = `
-      <button id="btnEdit" type="button">수정</button>
-      <button id="btnDelete" type="button">삭제</button>
-    `;
-
-    const $btnDelete = $btns.querySelector('#btnDelete');
-    const $btnEdit = $btns.querySelector('#btnEdit');
-
-    //수정
-    $btnEdit.addEventListener('click', e => {
-      changeEditMode(frm);
-    });
-
-    //삭제
-    $btnDelete.addEventListener('click', e => {
-      const pid = frm.productId.value;
-      if (!pid) {
-        alert('상품조회 후 삭제바랍니다.');
-        return;
-      }
-
-      if (!confirm('삭제하시겠습니까?')) return;
-      delProduct(pid, frm);
-    });
-  };
-}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//댓글목록 화면
-function displayPostCommentList(postComment) {
 
   const makeTr = postComment => {
     const $tr = postComment
       .map(
         postComment =>
-          `<tr data-cid=${postComment.commentId}>
+          `<tr id="comment-${postComment.commentId}">
             <td>${postComment.commentId}</td>
             <td>${postComment.content}</td>
             <td>${postComment.nickname}</td>
             <td>${postComment.cdate}</td>
             <td>${postComment.udate}</td></tr>
-            <tr>
-            <td><span class="field-error client" id="errCommentSave"></span></td>
-            <td><span class="field-error client" id="errCommentDel"></span></td>
-            <td>
-              <button class="btnEditComment">수정</button>
-              <button class="btnDeleteComment">삭제</button>
-            </td></tr>`,
+          <tr data-cid="${postComment.commentId}">
+            <td colspan="3"><span class="field-error client" id="errContent-${postComment.commentId}"></span></td>
+            <td colspan="2" style="text-align: right;">
+            <button class="btnEditComment">수정</button>
+            <button class="btnDeleteComment">삭제</button>
+            </td>
+          </tr>`,
       )
       .join('');
     return $tr;
   };
-  const $commentFormWrap = document.createElement('div');
-  $commentFormWrap.innerHTML = `
+
+  $list.innerHTML = `
     <table>
       <caption> 게 시 글 목 록 </caption>
       <thead>
@@ -453,111 +362,6 @@ function displayPostCommentList(postComment) {
       </tbody>
     </table>`;
 
-  const $postComment = $commentFormWrap.querySelectorAll('table tbody tr');
+  const $postComment = $list.querySelectorAll('table tbody tr');
   attachCommentHandlers();
 }
-
-$commentFormWrap.setAttribute('id','list')
-document.body.appendChild($commentFormWrap);
-
-const divEle = document.createElement('div');
-divEle.setAttribute('id','reply_pagenation');
-document.body.appendChild(divEle);
-
-async function configPagination(){
-  const url = `/api/postBoards/${pid}/postComment/totCnt`;
-  try {
-    const result = await ajax.get(url);
-
-    const totalRecords = result.body; // 전체 레코드수
-
-    const handlePageChange = (reqPage)=>{
-      return getPostComment(reqPage,recordsPerPage);
-    };
-
-    // Pagination UI 초기화
-    var pagination = new PaginationUI('reply_pagenation', handlePageChange);
-
-    pagination.setTotalRecords(totalRecords);       //총건수
-    pagination.setRecordsPerPage(recordsPerPage);   //한페이지당 레코드수
-    pagination.setPagesPerPage(pagesPerPage);       //한페이지당 페이지수
-
-    // 첫페이지 가져오기
-    pagination.handleFirstClick();
-
-  }catch(err){
-    console.error(err);
-  }
-}
-
-// 버튼 클릭 핸들러 연결 함수
-function attachCommentHandlers() {
-  document.querySelectorAll('#list tbody tr').forEach($tr => {
-    const commentId = $tr.dataset.cid;
-
-    // 삭제 버튼
-    $tr.querySelector('.btnDeleteComment').onclick = () => {
-      if (!confirm('댓글을 정말 삭제하시겠습니까?')) return;
-      delPostComment(pid, commentId);
-    };
-
-    // 수정 버튼 (예시: prompt로 새 내용 입력)
-    $tr.querySelector('.btnEditComment').onclick = async () => {
-      frm.querySelector('#errCommentSave').textContent = '';
-      frm.querySelector('#errCommentDel').textContent = '';
-
-      const formData = new FormData(frm); //폼데이터가져오기
-      const postBoard = {};
-
-      [...formData.keys()].forEach(
-        ele => (postBoard[ele] = formData.get(ele))
-      );
-
-      const result = await modifyPostBoard(postBoard.postId, postBoard);
-
-      if (result.header.rtcd.startsWith('E')) {
-        const details = result.header.details;
-        if (details.title)  frm.querySelector('#errTitle').textContent   = details.title;
-        if (details.content) frm.querySelector('#errContent').textContent = details.content;
-        return;
-      }
-      const udate = result.body.udate;
-      frm.querySelector('input[name="udate"]').value = udate; //수정
-      changeReadMode(frm); //읽기모드
-    };
-  });
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
